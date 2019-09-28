e5549718516537998fd12a1fda049bcb579d0843
bob <bob@bobs-MacBook-Pro.local>
2019-09-28 23:49:13 +0700

    added redux, and implemented userReducer and userActions to header and app components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+  console.log(userRef);
 
   const snapshot = await userRef.get();
 
