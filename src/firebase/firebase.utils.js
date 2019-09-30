69a20169b136723080f91ce3b0239be2d04b6e84
bob <bob@bobs-MacBook-Pro.local>
2019-09-30 11:21:34 +0700

    added checkout page, checkout item component and updated cart to use selectors

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,7 +17,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
-  console.log(userRef);
 
   const snapshot = await userRef.get();
 
